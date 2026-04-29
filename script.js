ff47b243ce79b3a4fa723c3140da3fb584840e2b
Aceuzc <aceeufracio23@email.com>
2026-04-29 17:44:22 +0800

    Committed

---
+++ script.js
@@ -0,0 +1,27 @@
+
+document.getElementById("homeBtn").addEventListener("click", () => {
+    document.getElementById("hero").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
+document.getElementById("projectsBtn").addEventListener("click", () => {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
+document.getElementById("skillsBtn").addEventListener("click", () => {
+    document.getElementById("skills").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
+document.getElementById("contactBtn").addEventListener("click", () => {
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
+});
